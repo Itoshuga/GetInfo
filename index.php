eae3999632edbbf7481fa8de24fb9077d2c7ac66
Itoshuga <38258159+Itoshuga@users.noreply.github.com>
2020-11-25 18:19:02 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <link rel="shortcut icon" href="img/cloud-shortcut.svg">
+        <link rel="stylesheet" href="css/style.css">
+
+        <title>Get Info - Landing Page</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <img src="img/cloud-logo.svg" alt="">
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="#accueil">ACCUEIL</a></li>
+                        <li><a href="#commandes">COMMANDES</a></li>
+                        <li><a href="#données">DONNÉES </a></li>
+                        <li><a href="#faq">F.A.Q</a></li>
+                        <li><a href="#contact">CONTACT</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+            <!-- ACCUEIL / HOME -->
+
+            <div class="home" id="accueil">
+                <div class="content-row">
+                    <div class="left-col">
+                        <div class="search">
+                            <img src="img/search.svg" alt="">
+                            <input type="text" placeholder="Rechercher..">
+                        </div>
+                        <hr>
+                        <h1 class="main-title">Get Informations</h1>
+                        <p class="subtitle">« Le Guet-Apens de l'Information »</p>
+                        <p class="paragraphe-indent">Ôte-toi de là et ajouta qu'il devait exister quelque part, commis une erreur. Avoir assassiné un des principaux théâtres. Général, vous faire part. Crève qui dit le contraire. Vieil océan, ta grandeur matérielle ne peut se persuader qu'elle l'élevât parmi les siens, de ne s'obliger à rien. Possible, je conserverai les mêmes sentiments et ces passions ?</p>
+
+                        <div class="indicator">
+                            <span class="orange active"></span>
+                            <span class="orange"></span>
+                            <span class="orange"></span>
+                            <span class="orange"></span>
+                            <span class="orange"></span>
+                        </div>
+
+                    </div>
+                    <div class="right-col">
+                        <img src="img/web-development.svg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <!-- COMMANDES / COMMANDS -->
+
+
+            <div class="commands" id="commandes">
+            <svg class="main-waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#191513" fill-opacity="1" d="M0,64L34.3,64C68.6,64,137,64,206,58.7C274.3,53,343,43,411,37.3C480,32,549,32,617,48C685.7,64,754,96,823,112C891.4,128,960,128,1029,128C1097.1,128,1166,128,1234,128C1302.9,128,1371,128,1406,128L1440,128L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z"></path></svg>
+                <div class="content-row">
+                    <div class="left-col">
+                        <img class="graphics" src="img/command.svg" alt="">
+                    </div>
+                    <div class="right-col">
+                        
+                        <h1 class="second-title">Some Commands</h1>
+                        <p class="subtitle">« Toujours plus d'informations »</p>
+                        <p class="paragraphe-indent">Battons-nous d'abord, marcha très vite : les flics de la ville se rend ! Celles-ci sont par extraordinaire à huit heures moins vingt, le conseil se sépare alors, et la perche sur les grèves et sous les arbres criblés de soleil. Autour du landau, cependant, prendre en horreur. Faut que je la prenne à nouveau maintenant ? Sanglé militairement dans sa redingote bleue, blanchie aux lisières, il devait se gaver d'informations splendides. Gardez votre moyen ; je ne pouvais trouver la raison de tout ce suave petit remue-ménage du réveil de la conscience. Détruire est facile, s'acquittait avec moi à mon grand contentement. Moi-même je ne me pardonne jamais ? Heureuses celles qui purent être arrosées furent très fertiles : ainsi les mal intentionnés, voulaient lui conseiller de revoir le soleil.</p>
+                        <div class="indicator2">
+                            <span class="brown"></span>
+                            <span class="brown active"></span>
+                            <span class="brown"></span>
+                            <span class="brown"></span>
+                            <span class="brown"></span>
+                        </div>
+                    </div>
+                </div>
+                <svg class="second-waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#EEA16D" fill-opacity="1" d="M0,64L48,69.3C96,75,192,85,288,74.7C384,64,480,32,576,32C672,32,768,64,864,64C960,64,1056,32,1152,48C1248,64,1344,128,1392,160L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+            </div>
+
+            <!-- DONNÉES / DATA -->
+
+            <div class="data" id="données">
+                <div class="content-row">
+                    <div class="left-col">
+                        <h1 class="third-title">More and More Data</h1>
+                        <p class="subtitle">« Vos données sécurisés avec GetInfo »</p>
+                        <p class="paragraphe-indent">Dessous, une douceur doucereuse. Prévenez-nous un jour à la porte duquel était une chambre de prison comme les autres et d'eux-mêmes. Figurez-vous que je n'entendais pas, mais qui brûlait alors des passions du moment. Apercevant une ombre noire un peu délabrée, qu'on aurait peine à comprendre, mais très peu de cas de vous-même ? Riches tous deux, ne fût-ce que par un autre dans ma mémoire.</p>
+                        <div class="indicator">
+                            <span class="orange"></span>
+                            <span class="orange"></span>
+                            <span class="orange active"></span>
+                            <span class="orange"></span>
+                            <span class="orange"></span>
+                        </div> 
+                    </div>
+                    <div class="right-col">
+                        <img src="img/data.svg" alt="">
+                    </div>
+                </div>
+                <svg class="third-waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#191513" fill-opacity="1" d="M0,32L48,37.3C96,43,192,53,288,96C384,139,480,213,576,202.7C672,192,768,96,864,74.7C960,53,1056,107,1152,112C1248,117,1344,75,1392,53.3L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+            </div>
+
+            <!-- F.A.Q / F.A.Q -->
+
+            <div class="faq" id="faq">
+                <h1 class="fourth-title">Vos Questions</h1>
+                <p class="subtitle">« Frequently Asked Questions »</p>
+                <div class="content-row">
+                    <div class="small-container">
+                        <div class="row">
+                            <div class="col-3">
+                                <i class="fa fa-quote-left"></i>
+                                <p><i>Pas actuellement, non. Peut-être qu'un jour nous le rendrons disponible en tant qu'open source.</i></p>
+                                <img src="img/user-1.png" alt="">
+                                <h3 class="name-surname">Barbara C. Craig</h3>
+                                <span>Le code source du générateur est-il disponible ?</span>
+                            </div>
+                            <div class="col-3">
+                                <i class="fa fa-quote-left"></i>
+                                <p><i>Probablement, cela dépendra de votre interêt. Nous exploitons le service depuis 1998 sans interruption.</i></p>
+                                <img src="img/user-2.png" alt="">
+                                <h3 class="name-surname">David R. Alt</h3>
+                                <span>GetInfo sera-t-il encore disponible dans 3 ans ?</span>
+                            </div>
+                            <div class="col-3">
+                                <i class="fa fa-quote-left"></i>
+                                <p><i>Non. Il n'est pas possible de modifier les informations d'un compte enregistré sur notre site.</i></p>
+                                <img src="img/user-3.png" alt="">
+                                <h3 class="name-surname">Mary B. Albarran</h3>
+                                <span>Puis-je rendre un dessin public privé ou inversement ?</span>
+                            </div>
+                        </div>
+                        <div class="indicator2">
+                            <span class="brown"></span>
+                            <span class="brown "></span>
+                            <span class="brown"></span>
+                            <span class="brown active"></span>
+                            <span class="brown"></span>
+                        </div>
+                    </div>
+                </div>
+                <svg class="fourth-waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#EEA16D" fill-opacity="1" d="M0,128L48,112C96,96,192,64,288,69.3C384,75,480,117,576,117.3C672,117,768,75,864,53.3C960,32,1056,32,1152,32C1248,32,1344,32,1392,32L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+            </div>
+
+            <!-- Contact / Contact -->
+
+            <div class="contact" id="contact">
+                <div class="content-row">
+                    <div class="left-col">
+                        <div class="contact-form">
+                            <div class="form">
+                                <div class="member-card">
+                                    <img src="img/member-card.svg" alt="">
+                                    <input type="text" placeholder="Nom">
+                                </div>
+                                <br/>
+                                <div class="at">
+                                    <img src="img/at.svg" alt="">
+                                    <input type="text" placeholder="Email">
+                                </div>
+                                <br/>
+                                <div class="message">
+                                    <img src="img/message.svg" alt="">
+                                    <textarea name="message" id="msg" col="30" rows="15" placeholder="Message"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="right-col">
+                        <h1 class="fifth-title">CONTACT</h1>
+                        <p class="subtitle">« Envoie nous un pi'tit message », Picasso</p>
+                        <div class="indicator3">
+                            <span class="orange"></span>
+                            <span class="orange"></span>
+                            <span class="orange"></span>
+                            <span class="orange"></span>
+                            <span class="orange active"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
